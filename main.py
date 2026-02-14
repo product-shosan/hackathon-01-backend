3b04022c381eb7a59b51d5713dd8707693bbadf0
Haruto Matsuda <june27harutoex@gmail.com>
2026-02-14 15:25:34 +0900

    MotionSensorは4,17とかを使うといいらしい

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from gpiozero import MotionSensor
 from signal import pause
 import time
 
-print("Motion Detected!!")
+sensor = MotionSensor(4)
 
-#関数の設定
 def detected():
-    a = 'Motion Deteced!!'
-    print(a)
+    print(f"[{time.strftime('%H:%M:%S')}] Motion Detected!! 人を検知しました！")
 
-sensor = MotionSensor(2)
+def m_stopped():
+    print("動きが止まりました")
 
+# イベントの登録
 sensor.when_motion = detected
+sensor.when_no_motion = m_stopped # 検知が終了したとき用
 
-while 1:
-    print('.')
-    time.sleep(0.1)
+print("センサー監視開始... (Ctrl+C で終了)")
 
-pause()
+try:
+    pause()
+except KeyboardInterrupt:
+    print("\n終了します")
