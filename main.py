5a966d6a3aa52952c8a9742c42713d74a1a9d8bf
Haruto Matsuda <june27harutoex@gmail.com>
2026-02-14 16:22:10 +0900

    FastAPIの導入とGET関数の作成

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 from gpiozero import MotionSensor
 from signal import pause
 import time
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+#状態管理用変数
+status = {
+    "is_occupied": False
+}
 sensor = MotionSensor(4)
 
+#検知用関数
 def detected():
-    print(f"[{time.strftime('%H:%M:%S')}] Motion Detected!! 人を検知しました！")
+    status["is_occupied"] = True
+    print("人を検知しました。")
 
 def m_stopped():
-    print("動きが止まりました")
+    status["is_occupied"] = False
+    print("人を検知しました。")
 
-# イベントの登録
+#関数の割り当て
 sensor.when_motion = detected
-sensor.when_no_motion = m_stopped # 検知が終了したとき用
+sensor.when_no_motion = m_stopped 
+
+
+#APIの作成
+app = FastAPI()
+
+#CORSの許可（一旦全許可）
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+#GETリクエストの作成
+@app.get("/status")
+def get_status():
+    return status
 
 print("センサー監視開始... (Ctrl+C で終了)")
 
